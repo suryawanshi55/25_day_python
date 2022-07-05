3c2ede6cb5fbfb9be79bbfe04a57e078da58b392
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-07-05 11:12:24 +0530

    Add files via upload

---
+++ escape_sequence.py
@@ -0,0 +1,58 @@
+# print('\f hii')    ♀ hii
+# print('hello wo\'rld')    hello wo'rld
+# print("""coding\n
+# enviroment""")coding  
+              
+#               enviroment
+# print('hi\anuja')     hinuja
+# print('hii\babita')     hiabita
+# print('hii\\babita')    hii\babita
+# print('hii\chitra')    hii\chitra
+# print('hii\\chitra')      hii\chitra
+# print('hii\disha')   hii\disha
+# print('hii\\disha' )    hii\disha
+# print('hii\eisha')   hii\eisha
+# print('hii\\eisha')   hii\eisha
+# print('hii\falguni')   hii♀alguni
+# print('hii\\falguni')   hii\falguni
+# print('hii\gautami')    hii\gautami
+# print('hii\\gautami')    hii\gautami
+# print('hii\heena')   hii\heena
+# print('hii\\heena')    hii\heena
+# print('hii\isha')     hii\isha
+# print('hii\\isha')    hii\isha
+# print('hii\jyoti')    hii\jyoti
+# print('hii\\jyoti')    hii\jyoti
+# print('hii\kalayani')    hii\kalayani
+# print('hii\\kranti')    hii\kranti
+# print('hii\lata')     hii\lata
+# print('hii\\lata')     hii\lata
+# print('hii\meena')        hii\meena
+# print('hii\\meena')    hii\meena
+# print('hii\neha')       hii
+#                         eha
+# print('hii\\neha')    hii\neha
+# print('hii\odisa')    hii\odisa
+# print('hii\\odisa')    hii\odisa
+# print('hii\pooja')    hii\pooja
+# print('hii\\pooja')     hii\pooja
+# print('hii\qwait')    hii\qwait
+# print('hii\\qwait')   hii\qwait
+# print('hii\rutu')utu
+# print('hii\\rutu')hii\rutu
+# print('hii\swati')   hii\swati
+# print('hii\\swati')    hii\swati
+# print('hii\tina')       hii     ina
+# print('hii\\tina')      hii\tina
+# print('hii\usha')        SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 3-4: truncated \uXXXX escape
+# print('hii\\usha')     hii\usha
+# print('hii\varsha')     hii♂arsha
+# print('hii\\varsha')     hii\varsha
+# print('hii\wina')hii\wina
+# print('hii\\wina')hii\wina
+# print('hii\xerox')      SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 3-5: truncated \xXX escape
+# print('hii\\xerox')       hii\xerox
+# print('hii\yevati')hii\yevati
+# print('hii\\yevati')hii\yevati
+# print('hii\zara')   hii\zara
+# print('hii\\zara')    hii\zara
